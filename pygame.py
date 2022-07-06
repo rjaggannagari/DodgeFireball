d46eca836367b0b67d9d848832329e81b50d267e
rjaggannagari <rjaggannagari@vt.edu>
2022-07-06 12:06:56 -0400

    basic function of the game

--- pygame.py
+++ pygame.py
@@ -1,8 +1,9 @@
 # imports 
 import turtle
 import random
+import time
 
-# creates the window
+# creates the game window
 window = turtle.Screen()
 window.title('Dodge the Object')
 window.bgpic('space_bg.gif')
@@ -17,15 +18,47 @@ space_ship.shape("alien.gif")
 space_ship.color("white")
 space_ship.penup()
 space_ship.goto(0, 0)
-space_ship.speed(5)
+space_ship.speed(10)
 
-# meteor object
-meteor = turtle.Turtle()
-meteor.shape("fire.gif")
-meteor.penup()
-# random x coordinate to get spawned into
-random_x = random.randint(-400, 400)
-meteor.goto(random_x, 300)
+# the score starts at 0
+score = 0
+#scoreboard object
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0".format(score), align="center", font=("Courier", 24, "normal"))
+
+meteors = []
+# meteor objects
+for _ in range (10):
+    meteor = turtle.Turtle()
+    meteor.shape("fire.gif")
+    meteor.speed(10)
+    meteor.penup()
+    # random x coordinate to get spawned into
+    random_x = random.randint(-400, 400)
+    meteor.goto(random_x, 300)
+    meteor.speed(random.uniform(0.4, 0.8))
+    meteors.append(meteor)
+    time.sleep(0.1)
+
+below_meteors = []
+# below meteor objects
+for _ in range (10):
+    below_meteor = turtle.Turtle()
+    below_meteor.shape("fire.gif")
+    below_meteor.speed(10)
+    meteor.penup()
+    # random x coordinate to get spawned into
+    random_x = random.randint(-400, 400)
+    below_meteor.goto(random_x, -300)
+    below_meteor.speed(random.uniform(0.4, 0.8))
+    below_meteors.append(below_meteor)
+    time.sleep(0.1)
 
 # adds movement to the right
 def move_right():
@@ -58,20 +91,51 @@ window.onkeypress(move_left, "a")
 window.onkeypress(move_up, "w")
 window.onkeypress(move_down, "s")
 
-while True:
+# only runs when it is true
+game_running = True
+while game_running:
     window.update()
 
-    # sets a speed up for the meteor to fly down
-    old_meteor_y = meteor.ycor()
-    new_meteor_y = old_meteor_y - 0.1
-    meteor.sety(new_meteor_y)
+    # the top meteors
+    for meteor in meteors:
+        new_y = meteor.ycor()
+        new_y -= random.uniform(0.2, 0.8)
+        meteor.sety(new_y)
 
-    # this gives us a new random x value for the meteor
-    new_random_x = random.randint(-400, 400)
-    # respawns the fireball at a new location
-    if meteor.ycor() < -300:
-        meteor.goto(new_random_x, 300)
+        # this checks to see if there is a collision between the ship and the meteor
+        if space_ship.distance(meteor) < 25:
+            print("Game Over")
+            time.sleep(2)
+            game_running = False
 
-    if space_ship.distance(meteor) < 25:
-        print("You Lost")
-        window.resetscreen()
+        # this gives us a new random x value for the meteor
+        new_random_x = random.randint(-390, 390)
+
+        # respawns the fireball at a new location
+        if meteor.ycor() < -300:
+            meteor.goto(new_random_x, 300)
+            score = score + 1
+            pen.clear()
+            pen.write("Score: {} ".format(score), align="center", font=("Courier", 24, "normal"))
+
+    # the bottom meteors
+    for below_meteor in below_meteors:
+        new_below_y = below_meteor.ycor()
+        new_below_y += random.uniform(0.2, 0.8)
+        below_meteor.sety(new_below_y)
+
+        # this checks to see if there is a collision between the ship and the meteor
+        if space_ship.distance(below_meteor) < 25:
+            print("Game Over")
+            time.sleep(2)
+            game_running = False
+
+        # this gives us a new random x value for the meteor
+        new_bottom_random_x = random.randint(-390, 390)
+
+        # respawns the fireball at a new location
+        if below_meteor.ycor() > 300:
+            below_meteor.goto(new_bottom_random_x, -300)
+            score = score + 1
+            pen.clear()
+            pen.write("Score: {} ".format(score), align="center", font=("Courier", 24, "normal"))
